4c62c99cc51f5e7b01132da40de43aad4e3a3870
NaLDo627 <naruhodo627@gmail.com>
2020-10-08 00:31:34 +0900

    fix: timezone 설정

--- src/main/java/net/hkpark/cockstalgia/CockstalgiaCoreApplication.java
+++ src/main/java/net/hkpark/cockstalgia/CockstalgiaCoreApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class CockstalgiaCoreApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CockstalgiaCoreApplication.class, args);
