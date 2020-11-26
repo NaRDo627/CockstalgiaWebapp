e5d7ab3eac07968c8c5f0d6ad3a6e729de8e3e4b
NaLDo627 <naruhodo627@gmail.com>
2020-11-26 10:16:02 +0900

    test: security 테스트 추가

---
+++ src/test/java/net/hkpark/cockstalgia/webview/admin/controller/WebViewAdminControllerTest.java
@@ -0,0 +1,72 @@
+package net.hkpark.cockstalgia.webview.admin.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WebViewAdminControllerTest {
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void 관리자_페이지_접속_시도_리다이렉션_테스트() throws Exception {
+        // given
+        String url = "/admin";
+
+        // when
+        mvc.perform(get(url))
+
+        // then
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void 관리자_페이지_접속_시도_비인가_테스트() throws Exception {
+        // given
+        String url = "/admin";
+
+        // when
+        mvc.perform(get(url))
+
+                // then
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void 관리자_페이지_접속_시도_인가_테스트() throws Exception {
+        // given
+        String url = "/admin";
+
+        // when
+        mvc.perform(get(url))
+
+                // then
+                .andExpect(status().isOk());
+    }
+}
